3401101d8a1bb4c47366b965cbf7bbcc227f5ab1
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-08-03 13:18:13 +0700

    fix: refactor for better readability

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,11 @@
 function Button({ onClick } : { onClick: () => void }) {
     return (
-    <button 
-        onClick={onClick} 
-        className={`outline-none w-[15cqi] aspect-400/100 bg-[url('./wood-button.svg')] bg-contain bg-center bg-no-repeat font-darumadrop-one text-[2cqi] pb-[1cqh] text-white`}>
+    <button onClick={onClick} className={`
+        outline-none 
+        w-[15cqi] aspect-400/100 pb-[1cqh]
+        bg-[url('/wood-button.svg')] bg-contain bg-center bg-no-repeat 
+        font-darumadrop-one text-[2cqi] text-white
+    `}>
         Wish
     </button>
 )}
